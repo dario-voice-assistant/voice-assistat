b8112ddfc44181e59f145118145221c5064e049b
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-08-23 17:33:20 +0300

    feature: Design gui & button talk

--- dario/main.py
+++ dario/main.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 import audiomath as am
 import webbrowser
-import pyttsx3 
+import pyttsx3
 import datetime
 import smtplib
 import requests
@@ -10,53 +10,56 @@ from tkinter import *
 
 r = sr.Recognizer()
 
-test=pyttsx3.init()
+test = pyttsx3.init()
+
+
 def dario_speek(audio):
     test.say(audio)
     test.runAndWait()
 
 
 def search():
-    search=record_audio('what do you want to search for')
+    search = record_audio('what do you want to search for')
     url = f'https://google.com/search?q={search}'
     webbrowser.get().open(url)
     dario_speek(f'this is what I found about {search}')
+
 
 def location():
     location = record_audio('what is the location that you want e to search for ??')
     url = f'https://google.nl/maps/place/{location}/&amp;'
     webbrowser.get().open(url)
 
+
 def python():
-    temp=record_audio('what would you want me to search for')
-    if ('for'or 'loops')in temp:
-        url='https://www.w3schools.com/python/python_for_loops.asp'
+    temp = record_audio('what would you want me to search for')
+    if ('for' or 'loops') in temp:
+        url = 'https://www.w3schools.com/python/python_for_loops.asp'
         webbrowser.get().open(url)
-    if ('function')in temp:
-        url='https://www.w3schools.com/python/python_functions.asp'
+    if ('function') in temp:
+        url = 'https://www.w3schools.com/python/python_functions.asp'
         webbrowser.get().open(url)
-    if ('variables')in temp:
-        url='https://www.w3schools.com/python/python_variables.asp'
+    if ('variables') in temp:
+        url = 'https://www.w3schools.com/python/python_variables.asp'
         webbrowser.get().open(url)
-    if ('data'and'types')in temp:
-        url='https://www.w3schools.com/python/python_datatypes.asp'
+    if ('data' and 'types') in temp:
+        url = 'https://www.w3schools.com/python/python_datatypes.asp'
         webbrowser.get().open(url)
-    if ('operators')in temp:
-        url='https://www.w3schools.com/python/python_operators.asp'
+    if ('operators') in temp:
+        url = 'https://www.w3schools.com/python/python_operators.asp'
         webbrowser.get().open(url)
-    if ('classes'or'objects')in temp:
-        url='https://www.w3schools.com/python/python_classes.asp'
+    if ('classes' or 'objects') in temp:
+        url = 'https://www.w3schools.com/python/python_classes.asp'
         webbrowser.get().open(url)
-    if ('list'or'lists')in temp:
-        url='https://www.w3schools.com/python/python_lists.asp'
+    if ('list' or 'lists') in temp:
+        url = 'https://www.w3schools.com/python/python_lists.asp'
         webbrowser.get().open(url)
-    if ('if'or'else' or 'conditions')in temp:
-        url='https://www.w3schools.com/python/python_conditions.asp'
+    if ('if' or 'else' or 'conditions') in temp:
+        url = 'https://www.w3schools.com/python/python_conditions.asp'
         webbrowser.get().open(url)
 
 
 def start():
-   
     hour = datetime.datetime.now().hour
     if hour >= 6 and hour < 12:
         dario_speek("Good morning sir")
@@ -69,16 +72,16 @@ def start():
 
     dario_speek("Dario at your service please tell me how can I help you?")
 
+
 def record_audio(ask=False):
     if ask:
-        dario_speek(ask)  
-    s=am.Record(4)
-    s.Write('some_file.wav')  
+        dario_speek(ask)
+    s = am.Record(4)
+    s.Write('some_file.wav')
     with sr.AudioFile('some_file.wav') as source:
         audio = r.listen(source)
-        
-       
-        voice_data=''
+
+        voice_data = ''
         try:
             voice_data = r.recognize_google(audio, language='en-in')
         except sr.UnknownValueError:
@@ -86,6 +89,7 @@ def record_audio(ask=False):
         except sr.RequestError:
             print('sorry: my speech service is down')
         return voice_data
+
 
 def respond(audio):
     if ('what is your name' or 'name') in audio:
@@ -107,12 +111,12 @@ def respond(audio):
         get_weather(audio)
     if 'python' in audio:
         python()
-    if "play music" in voice_data:
-        song_path = 'D:\\Music'
-        songs = os.listdir(song_path)
-        os.startfile(os.path.join(song_path, songs[0]))
+    # if "play music" in voice_data:
+    #     song_path = 'D:\\Music'
+    #     songs = os.listdir(song_path)
+    #     os.startfile(os.path.join(song_path, songs[0]))
 
- 
+
 def sender():
     try:
         dario_speek("yes sir, what should I say?")
@@ -125,15 +129,17 @@ def sender():
         print(e)
         dario_speek("Unable to send the email")
 
+
 def send_email(to, content):
-    email_server = smtplib.SMTP('smtp.gmail.com',587)
+    email_server = smtplib.SMTP('smtp.gmail.com', 587)
     email_server.ehlo()
     email_server.starttls()
     gmail_user = 'dariovapy@gmail.com'
     gmail_password = 'dariova123'
-    email_server.login(gmail_user,gmail_password)
-    email_server.sendmail(gmail_user,to,content)
+    email_server.login(gmail_user, gmail_password)
+    email_server.sendmail(gmail_user, to, content)
     email_server.quit()
+
 
 def get_date():
     year = int(datetime.datetime.now().year)
@@ -146,14 +152,15 @@ def get_date():
     dario_speek(full_month_name)
     dario_speek(year)
 
+
 def get_time():
     now = datetime.datetime.now()
-    current_time= now.strftime("%I:%M %p")
+    current_time = now.strftime("%I:%M %p")
     dario_speek('The current time is')
     dario_speek(current_time)
 
-def get_weather(audio):
 
+def get_weather(audio):
     temp = record_audio("what is the city name")
     city_name = temp.lower()
     api_key = "39577f16323c466893c05341fcc378c6"
@@ -164,15 +171,29 @@ def get_weather(audio):
     temp = data["data"][0]["high_temp"]
     dario_speek(f" the temperature in {city_name} is {temp} ")
 
-# start()
-# while True:
-#     voice_data=record_audio()
-#     respond(voice_data)
+
+def button_talker():
+    start()
+    while True:
+        voice_data = record_audio()
+        respond(voice_data)
+
 
 def application_GUI():
     root = Tk()
-    root.geometry("350x275")
+
+
     root.title("Dario Voice Assistant")
+
+    root.config(bg='yellow')
+    img = PhotoImage(file="particles-2-gifffy.gif", format='gif')
+    label = Label(
+        root,
+        image=img
+    )
+    # sdfsdfsdffsfsfdfd
+    label.place(x=0, y=0, relwidth=1.1, relheight=1.1)
+    root.geometry("700x500")
     root.columnconfigure(0, weight=1)
     root.columnconfigure(1, weight=1)
     root.columnconfigure(2, weight=1)
@@ -180,18 +201,18 @@ def application_GUI():
     root.rowconfigure(1, weight=1)
     root.rowconfigure(2, weight=1)
     root.rowconfigure(3, weight=1)
+    # root.configure(bg='blue')
 
-    # Creating buttons
-    button1 = Button(root, text='Exit', command=root.quit)
-    button2 = Button(root, text='Hello')
-    button3 = Button(root, text='Hello')
+    button1 = Button(root, text='Exit', command=root.quit, height=1, width=10, bg='#001E6C', fg='#F9F9F9')
+    button2 = Button(root, text="Let's Talk", command=button_talker, height=1, width=10, bg='#001E6C', fg='#F9F9F9')
+    button3 = Button(root, text='Features', height=1, width=10, bg='#001E6C', fg='#F9F9F9')
+    # button3.config(bg='white')
 
-    # Placing buttons on grid
     button1.grid(row=3, column=0)
     button2.grid(row=3, column=1)
     button3.grid(row=3, column=2)
 
-
     root.mainloop()
-
+# # # # # # # #
+# # ## ## # ## #
 application_GUI()
